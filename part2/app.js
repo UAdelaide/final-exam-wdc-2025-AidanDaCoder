0f570264376eeb04971687d1c88bfe7ad037e6b8
aidan <a1924235@adelaide.edu.au>
2025-06-20 11:33:42 +0000

    [automatic commit] 2025-06-20 11:33:41 UTC 120.159.195.127

--- part2/app.js
+++ part2/app.js
@@ -132,7 +132,7 @@ app.get('/', (req, res) => {
         if (req.session.user.role === 'owner') {
             return res.redirect('/owner-dashboard');
         } else if (req.session.user.role === 'walker') {
-            return res.redirect('/walker-dashboard');
+         return res.redirect('/walker-dashboard');
         }
     }
     // If not logged in, or role is unknown (shouldn't happen with proper login)
