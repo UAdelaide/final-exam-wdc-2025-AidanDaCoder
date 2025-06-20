da208d074c36b50b74139911b8247b481fd6d078
aidan <a1924235@adelaide.edu.au>
2025-06-20 06:21:52 +0000

    [automatic commit] 2025-06-20 06:21:51 UTC 120.159.195.127

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+import 'dotenv/config';
+import { pool, initializeDatabase, getConnection } from './db.js';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
