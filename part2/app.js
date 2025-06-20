328044912b581151e61807ef9ea0f3f7345df13f
aidan <a1924235@adelaide.edu.au>
2025-06-20 11:22:31 +0000

    [automatic commit] 2025-06-20 11:22:30 UTC 120.159.195.127

--- part2/app.js
+++ part2/app.js
@@ -35,7 +35,7 @@ app.use(session({
     cookie: {
         secure: process.env.NODE_ENV === 'production', // Use secure cookies in production if using HTTPS
         httpOnly: true,
-        maxAge: 100000// Cookie valid for 1 day (milliseconds)
+        maxAge: 1000000//cookie valid for a time of 1 million milliseconds
     }
 }));
 
